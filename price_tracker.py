f21bcdb4e6568d681410c4b61b71daee14a1e499
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-05-04 22:33:52 +0530

    sheets integration

--- price_tracker.py
+++ price_tracker.py
@@ -2,23 +2,7 @@ import gspread
 import requests
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "client_secret.json", scope)
-client = gspread.authorize(creds)
-sheet = client.open('product_prices')
-#row = ["hey", "i ", "added", "this", "row"]
-#sheet.insert_row(row, 3)
-
-csv_data = open("data.csv", "r").read()
-client.import_csv(sheet.id, csv_data)
-
-
-urls = open("urls.txt").read().split("\n")
+from datetime import date
 
 
 def get_prices(urls):
@@ -33,4 +17,35 @@ def get_prices(urls):
     return dict
 
 
-# print(get_prices(urls))
+def add_to_sheets(dic, client):
+    sheet = client.open('product_prices').sheet1
+    row_count = len(sheet.get_all_values())
+    prod_names = list(dic.keys())
+    prices = list(dic.values())
+    prod_names.insert(0, "Products/Dates")
+    prices.insert(0, day)
+    #sheet.insert_row(prod_names, 2)
+    sheet.insert_row(prices,row_count+1)
+
+
+if __name__ == "__main__":
+    today = date.today()
+    day = today.strftime("%B %d, %Y")
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
+
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        "client_secret.json", scope)
+    client = gspread.authorize(creds)
+
+    urls = open("urls.txt").read().split("\n")
+
+    dic = get_prices(urls)
+    print(dic)
+    add_to_sheets(dic, client)
+    print("Google sheet updated Successfully!")
+    print("View the google sheet at- https://docs.google.com/spreadsheets/d/1-9eXx4mr4kexJ4CiMlkf4YP0QP4ixQZw6bJ2qQRP6w8/edit?usp=sharing")
