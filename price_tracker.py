17608ddadc36dc38bdf8933c8903bd7800cbcd43
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-05-04 19:50:06 +0530

    Integrated Google Sheets

--- price_tracker.py
+++ price_tracker.py
@@ -1,19 +1,37 @@
+import gspread
 import requests
 from bs4 import BeautifulSoup
-
-urls = ["https://mdcomputers.in/amd-hexa-core-ryzen-5-3600.html",
-        "https://mdcomputers.in/msi-rx-5700-xt-mech-oc-8gb-gddr6.html",
-        "https://mdcomputers.in/galax-rtx-2060-super-ex-1-click-oc-8gb-gddr6-26isl6mpx2ex.html"
-        ]
+from oauth2client.service_account import ServiceAccountCredentials
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "client_secret.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open('product_prices')
+#row = ["hey", "i ", "added", "this", "row"]
+#sheet.insert_row(row, 3)
 
-for url in urls:
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.content, "html.parser")
-    prod_name = soup.find(class_="title-product").get_text().strip()
-    price = soup.find(id="price-old")["content"].strip()
-    price = int(price.split(".")[0])
-    print(prod_name)
-    print(price)
+csv_data = open("data.csv", "r").read()
+client.import_csv(sheet.id, csv_data)
+
+
+
+urls = open("urls.txt").read().split("\n")
+
+
+def get_prices(urls):
+    dict = {}
+    for url in urls:
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        prod_name = soup.find(class_="title-product").get_text().strip()
+        price = soup.find(id="price-old")["content"].strip()
+        price = int(price.split(".")[0])
+        dict[prod_name] = price
+    return dict
+
+
+# print(get_prices(urls))
